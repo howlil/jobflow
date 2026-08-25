d0846dfa5de3888d4bbac37d2927cf38ceef300f
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-08-25 22:18:44 +0700

    feat: expose correction and recovery management in options

--- entrypoints/options/App.tsx
+++ entrypoints/options/App.tsx
@@ -1,10 +1,25 @@
-import { ProfilePage } from '../../src/ui/profile/ProfilePage';
-import { ChromeProfileRepository } from '../../src/infrastructure/storage/chrome-profile-repository';
 import { ChromeVaultClient } from '../../src/infrastructure/messaging/chrome-vault-client';
+import { ChromeCorrectionRepository } from '../../src/infrastructure/storage/chrome-correction-repository';
+import { ChromeProfileRepository } from '../../src/infrastructure/storage/chrome-profile-repository';
+import { CorrectionMemorySection } from '../../src/ui/corrections/CorrectionMemorySection';
+import { BackupRecoveryInspector } from '../../src/ui/profile/BackupRecoveryInspector';
+import { ProfilePage } from '../../src/ui/profile/ProfilePage';
 
-const repository = new ChromeProfileRepository();
+const profileRepository = new ChromeProfileRepository();
+const correctionRepository = new ChromeCorrectionRepository();
 const vaultClient = new ChromeVaultClient();
 
 export default function App() {
-  return <ProfilePage repository={repository} vaultClient={vaultClient} />;
+  return (
+    <>
+      <ProfilePage repository={profileRepository} vaultClient={vaultClient} />
+      <main className="profile-page">
+        <CorrectionMemorySection repository={correctionRepository} />
+        <BackupRecoveryInspector
+          repository={profileRepository}
+          onRestored={() => window.location.reload()}
+        />
+      </main>
+    </>
+  );
 }
